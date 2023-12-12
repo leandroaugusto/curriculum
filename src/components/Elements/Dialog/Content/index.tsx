554e5a2647bf8a67bbae80e0269c82ee42b56783
Leandro Costa <leandroacosta2008@gmail.com>
2023-12-12 15:39:33 -0300

    add more detailed description about me

--- src/components/Elements/Dialog/Content/index.tsx
+++ src/components/Elements/Dialog/Content/index.tsx
@@ -6,7 +6,7 @@ interface DialogContentProps {
 
 export default function DialogContent({ children }: DialogContentProps) {
   return (
-    <div className="rounded-xl bg-white w-1/2 h-1/5 grid grid-cols-3 md:grid-cols-5 md:py-16 relative">
+    <div className="rounded-xl bg-white w-1/2 h-1/5 grid grid-cols-3 md:grid-cols-5 pt-10 md:py-16 relative">
       <DialogCloseIcon />
       {children}
     </div>
